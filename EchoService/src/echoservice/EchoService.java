af2eb88b721d1fc52a67c9b3467b4b10837182a7
williamhancock328 <williamhancock328@gmail.com>
2023-03-20 18:27:46 -0400

    added kissel's demo code
    this is just temporary

--- EchoService/src/echoservice/EchoService.java
+++ EchoService/src/echoservice/EchoService.java
@@ -1,16 +1,45 @@
 package echoservice;
 
-/**
- *
- * @author willi
- */
-public class EchoService {
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.IOException;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class EchoService {
+  public static void main(String[] args)
+  {
+    try
+    {
+      ServerSocket server = new ServerSocket(5000);
+
+      // Loop forever handing connections.
+      while (true)
+      {
+        // Wait for a connection.
+        Socket sock = server.accept();
+
+        System.out.println("Connection received.");
+
+        // Setup the streams for use.
+        Scanner recv = new Scanner(sock.getInputStream());
+        PrintWriter send = new PrintWriter(sock.getOutputStream(), true);
+
+        // Get the line from the client.
+        String line = recv.nextLine();
+        System.out.println("Client said: " + line);
+
+        // Echo the line back.
+        send.println(line);
+
+        // Close the connection.
+        sock.close();
+      }
     }
-    
+    catch(IOException ioe)
+    {
+      ioe.printStackTrace();
+    }
+  }
+
 }
--- SSOClient/src/ssoclient/SSOClient.java
+++ SSOClient/src/ssoclient/SSOClient.java
@@ -1,16 +1,48 @@
 package ssoclient;
 
-/**
- *
- * @author willi
- */
-public class SSOClient {
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.IOException;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class SSOClient {
+  public static void main (String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    Socket sock;
+    Scanner recv = null;
+    PrintWriter send = null;
+
+    try
+    {
+      // Set up a connection to the echo server running on the same machine.
+      sock = new Socket("127.0.0.1", 5000);
+
+
+      // Set up the streams for the socket.
+      recv = new Scanner(sock.getInputStream());
+      send = new PrintWriter(sock.getOutputStream(), true);
     }
-    
+    catch(UnknownHostException ex)
+    {
+      System.out.println("Host is unknown.");
+      return;
+    }
+    catch(IOException ioe)
+    {
+      ioe.printStackTrace();
+    }
+
+    // Prompt the user for a string to send.
+    System.out.print("Write a short message: ");
+    String msg = scan.nextLine();
+
+    // Send the message to the server.
+    send.println(msg);
+
+    // Echo the response to the screen.
+    String recvMsg = recv.nextLine();
+    System.out.println("Server Said: " + recvMsg);
+  }
 }
