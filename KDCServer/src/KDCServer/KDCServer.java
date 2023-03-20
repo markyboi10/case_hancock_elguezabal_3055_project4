9f1d4034e46c588652d5d6833b5ea40067208f74
williamhancock328 <williamhancock328@gmail.com>
2023-03-20 18:20:20 -0400

    created projects for each application

--- KDCServer/src/KDCServer/KDCServer.java
+++ KDCServer/src/KDCServer/KDCServer.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package sso_service;
+package KDCServer;
 
 /**
  *
  * @author Mark Case
  */
-public class SSO_Service {
+public class KDCServer {
 
     /**
      * @param args the command line arguments
