41c3c392c099e19a14879332d4fc8c78e2b63e46
Mark Case <casem@merrimack.edu>
2023-03-20 23:54:45 -0400

    Testing with CHAP and SHA-256

--- KDCServer/src/KDCServer/KDCServer.java
+++ KDCServer/src/KDCServer/KDCServer.java
@@ -8,13 +8,79 @@ package KDCServer;
  *
  * @author Mark Case
  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
+
 public class KDCServer {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static String[] userAndPass = {"Alice", "123321"};
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        System.out.println(Arrays.toString(userAndPass));
+ 
+        try {
+            ServerSocket server = new ServerSocket(5000);
+
+            // Loop forever handing connections.
+            while (true) {
+                // Wait for a connection.
+                Socket sock = server.accept();
+
+                System.out.println("Connection received.");
+
+                // Setup the streams for use.
+                Scanner recv = new Scanner(sock.getInputStream());
+                PrintWriter send = new PrintWriter(sock.getOutputStream(), true);
+
+                // Random nonce         
+                SecureRandom random = new SecureRandom();
+                byte[] nonceBytes = new byte[32];
+                random.nextBytes(nonceBytes);
+                String nonce = Base64.getEncoder().encodeToString(nonceBytes);
+                
+                // Get the username line from the client.
+                String line = recv.nextLine();
+
+                // Check if user exists and demand password + send nonce if correct,
+                // error otherwise
+                if(line.equals(userAndPass[0])) {
+                    send.println("Enter your password below. Your nonce is: " + nonce);
+                } else {
+                    send.println("User Error, name not found");
+                    System.exit(0);
+                }
+              
+                // Get hashed pass and nonce from client, compare and validate to kdc version
+                String line2 = recv.nextLine();
+                String line3 = recv.nextLine();
+                send.println("Checking hash . . .");
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                byte[] hashPass = digest.digest(userAndPass[1].getBytes(StandardCharsets.UTF_8));
+                byte[] hashNonce = digest.digest(nonce.getBytes(StandardCharsets.UTF_8));
+                if(line2.equals(Arrays.toString(hashPass)) && line3.equals(Arrays.toString(hashNonce))) {
+                    send.println("You have been authenticated");
+                } else {
+                    send.println("You have denied");
+                }
+
+                // Close the connection.
+                sock.close();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
-    
+
 }
--- SSOClient/src/ssoclient/SSOClient.java
+++ SSOClient/src/ssoclient/SSOClient.java
@@ -5,44 +5,69 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SSOClient {
-  public static void main (String[] args)
-  {
-    Scanner scan = new Scanner(System.in);
-    Socket sock;
-    Scanner recv = null;
-    PrintWriter send = null;
 
-    try
-    {
-      // Set up a connection to the echo server running on the same machine.
-      sock = new Socket("127.0.0.1", 5000);
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner scan = new Scanner(System.in);
+        Socket sock;
+        Scanner recv = null;
+        PrintWriter send = null;
+
+        try {
+            // Set up a connection to the echo server running on the same machine.
+            sock = new Socket("127.0.0.1", 5000);
+
+            // Set up the streams for the socket.
+            recv = new Scanner(sock.getInputStream());
+            send = new PrintWriter(sock.getOutputStream(), true);
+        } catch (UnknownHostException ex) {
+            System.out.println("Host is unknown.");
+            return;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        // Prompt the user for a string to send.
+        System.out.print("Username: ");
+        String msg = scan.nextLine();
+
+        // Send username
+        send.println(msg);
 
 
-      // Set up the streams for the socket.
-      recv = new Scanner(sock.getInputStream());
-      send = new PrintWriter(sock.getOutputStream(), true);
+        // KDC checks username validity and if valid, demands password and gives a nonce
+        String recvMsg = recv.nextLine();
+        System.out.println("Server Said: " + "\n" + recvMsg);
+        
+        // Extract nonce
+        String ExtractedNonce = "";
+        Pattern pattern = Pattern.compile("nonce is: (\\S+)");
+        Matcher matcher = pattern.matcher(recvMsg);
+        if (matcher.find()) {
+            ExtractedNonce = matcher.group(1);
+        } else {
+            System.exit(0);
+        }
+        
+        // Client sends hashed password and nonce
+        String msg2 = scan.nextLine();
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] clientHashPass = digest.digest(msg2.getBytes(StandardCharsets.UTF_8));
+        byte[] clientHashNonce = digest.digest(ExtractedNonce.getBytes(StandardCharsets.UTF_8));
+        send.println(Arrays.toString(clientHashPass));
+        send.println(Arrays.toString(clientHashNonce));
+
+        // Server recieved hash pass and nonce and will check validity via comparison
+        String recvMsg2 = recv.nextLine();
+        System.out.println("Server Said: " + "\n" + recvMsg2);
+        String recvMsg3 = recv.nextLine();
+        System.out.println(recvMsg3);
     }
-    catch(UnknownHostException ex)
-    {
-      System.out.println("Host is unknown.");
-      return;
-    }
-    catch(IOException ioe)
-    {
-      ioe.printStackTrace();
-    }
-
-    // Prompt the user for a string to send.
-    System.out.print("Write a short message: ");
-    String msg = scan.nextLine();
-
-    // Send the message to the server.
-    send.println(msg);
-
-    // Echo the response to the screen.
-    String recvMsg = recv.nextLine();
-    System.out.println("Server Said: " + recvMsg);
-  }
 }
