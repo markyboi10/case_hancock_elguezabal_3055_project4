33694b6ab85967efd58e997d7d1ad47f16127754
williamhancock328 <williamhancock328@gmail.com>
2023-03-29 21:25:00 -0400

    crypto done-ish

--- KDCServer/src/KDCServer/crypto/GCMEncrypt.java
+++ KDCServer/src/KDCServer/crypto/GCMEncrypt.java
@@ -1,5 +1,6 @@
 package KDCServer.crypto;
 
+import java.nio.ByteBuffer;
 import javax.crypto.Cipher;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.NoSuchPaddingException;
@@ -14,43 +15,49 @@ import java.util.Base64;
 import java.security.InvalidAlgorithmParameterException;
 import java.nio.charset.StandardCharsets;
 import java.security.spec.InvalidKeySpecException;
-
+import merrimackutil.util.Tuple;
 
 public class GCMEncrypt {
-	public static void encrypt(String mkey) throws
-	    NoSuchAlgorithmException, NoSuchPaddingException,
-			InvalidKeyException, IllegalBlockSizeException,
-			BadPaddingException,  InvalidAlgorithmParameterException, InvalidKeySpecException
-	{
-		int tagSize = 128;
-		// Set up an AES cipher object.
-		Cipher aesCipher = Cipher.getInstance("AES/GCM/NoPadding");
 
-		// Get a key generator object.
-		KeyGenerator aesKeyGen = KeyGenerator.getInstance("AES");
+    public static Tuple<byte[], byte[]> encrypt(String mkey, long valTime, long createTime, String uName, String sName) throws
+            NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeyException, IllegalBlockSizeException,
+            BadPaddingException, InvalidAlgorithmParameterException, InvalidKeySpecException {
+        int tagSize = 128;
+        // Set up an AES cipher object.
+        Cipher aesCipher = Cipher.getInstance("AES/GCM/NoPadding");
 
-		// Set the key size to 128 bits.
-		aesKeyGen.init(128);
+        // Get a key generator object.
+        KeyGenerator aesKeyGen = KeyGenerator.getInstance("AES");
 
-		// Generate the session key.
-		Key aesKey = aesKeyGen.generateKey();
-                //generate the master key from the password.
-                Key mKey = scrypt.genKey(mkey);
-		// Generate the IV.
-		SecureRandom rand = new SecureRandom();
-		byte[] rawIv = new byte[16];		// Block size of AES.
-		rand.nextBytes(rawIv);					// Fill the array with random bytes.
-		GCMParameterSpec gcmParams = new GCMParameterSpec(tagSize, rawIv);
+        // Set the key size to 128 bits.
+        aesKeyGen.init(128);
 
-		// Put the cipher in encrypt mode with the specified key.
-		aesCipher.init(Cipher.ENCRYPT_MODE, mKey, gcmParams);
+        // Generate the session key.
+        Key aesKey = aesKeyGen.generateKey();
+        //generate the master key from the password.
+        Key mKey = scrypt.genKey(mkey);
+        // Generate the IV.
+        SecureRandom rand = new SecureRandom();
+        byte[] rawIv = new byte[16];		// Block size of AES.
+        rand.nextBytes(rawIv);					// Fill the array with random bytes.
+        GCMParameterSpec gcmParams = new GCMParameterSpec(tagSize, rawIv);
 
-		//encrypt the session key
-		byte[] ciphertext = aesCipher.doFinal(aesKey.getEncoded());
+        // Put the cipher in encrypt mode with the specified key.
+        aesCipher.init(Cipher.ENCRYPT_MODE, mKey, gcmParams);
+        aesCipher.updateAAD(longToBytes(createTime));
+        aesCipher.updateAAD(longToBytes(valTime));
+        aesCipher.updateAAD(uName.getBytes(StandardCharsets.UTF_8));
+        aesCipher.updateAAD(sName.getBytes(StandardCharsets.UTF_8));
+        //encrypt the session key
+        byte[] ciphertext = aesCipher.doFinal(aesKey.getEncoded());
+        return new Tuple<byte[], byte[]>(ciphertext, rawIv);
+        
+    }
 
-    System.out.println("Ciphertext: " + Base64.getEncoder().encodeToString(ciphertext));
-    System.out.println("Key: " + Base64.getEncoder().encodeToString(aesKey.getEncoded()));
-		System.out.println("IV: " + Base64.getEncoder().encodeToString(rawIv));
-		System.out.println("Tag Size: " + tagSize + " bits.");
-	}
+    private static byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
+    }
 }
