ebeb04927eb96334fb761d9ab5fdce1b4172c661
Mark Case <casem@merrimack.edu>
2023-03-31 12:55:05 -0400

    Updated scrypt and moved decryption/scrypt files, need to setup decryption

---
+++ SSOClient/src/ClientServerCrypto/scrypt.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClientServerCrypto;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import org.bouncycastle.jcajce.spec.ScryptKeySpec;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+/**
+ *
+ * @author MarkC
+ */
+public class scrypt {
+        /**
+     * Derives the AES-128 key from the password.
+     * @param password the password to derive the key from
+     * @return the AES-128 key
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     */
+    public static SecretKey genKey(String password, String uName) throws NoSuchAlgorithmException,
+            InvalidKeySpecException {
+        final int COST = 2048;          // A.K.A Iterations
+        final int BLK_SIZE = 8;
+        final int PARALLELIZATION = 1;  // Number of parallel threads to use.
+        final int KEY_SIZE = 128;
+        ScryptKeySpec scryptSpec;
+
+        // Register bouncy castle provider.
+        Security.addProvider(new BouncyCastleProvider());
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("SCRYPT");
+
+        // THIS NEEDS TO BE THE SAME AS THE ONE USED DURING ENCRYPTION!!!
+         //needs to be passed over
+        byte salt[] = uName.getBytes();
+        
+//            byte[] salt = new byte[16];
+//            SecureRandom rand = new SecureRandom();
+//            rand.nextBytes(salt);
+
+        // Derive an AES key from the password using the password. The memory
+        // required to run the derivation, in bytes, is:
+        //    128 * COST * BLK_SIZE * PARALLELIZATION.
+        // The password argument expects and array of charaters *not* bytes.
+        //
+        scryptSpec = new ScryptKeySpec(password.toCharArray(), salt, COST, BLK_SIZE,
+                PARALLELIZATION, KEY_SIZE);
+        
+        // Generate the secrete key.
+        SecretKey key = factory.generateSecret(
+                scryptSpec);
+        return key;
+    }
+    
+}
