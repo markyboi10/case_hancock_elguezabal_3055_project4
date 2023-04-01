e6d7d0a0250cbeab05a9497c03c21a38707adea8
Tonya E <luis1944alex@gmail.com>
2023-03-31 21:53:45 -0400

    Added run-kdc.bat and run-sso.bat files that work when ran in the case_hancock_elguezabal_3055_project4 file.

---
+++ run-kdc.bat
@@ -0,0 +1,4 @@
+cd KDCServer/dist/
+java -jar KDCServer.jar -c ../../kdc-config/config.json
+
+pause
---
+++ run-sso-client.bat
@@ -0,0 +1,4 @@
+cd SSOClient/dist/
+java -jar SSOClient.jar -h ../../hosts.json -u alice -s echoservice
+
+pause
